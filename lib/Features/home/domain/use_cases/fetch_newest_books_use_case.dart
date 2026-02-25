49d7c9554e3290d2aea69a5165b8951c2142b972
Abdulrahman <syamabdo382@gmail.com>
2026-02-25 03:11:21 +0200

    create FetchNewestBooksUseCase

---
+++ lib/Features/home/domain/use_cases/fetch_newest_books_use_case.dart
@@ -0,0 +1,16 @@
+import 'package:clean_arch_bookly/Features/home/domain/entites/book_entity.dart';
+import 'package:clean_arch_bookly/Features/home/domain/repos/home_repo.dart';
+import 'package:clean_arch_bookly/core/errors/failure.dart';
+import 'package:clean_arch_bookly/core/use_cases/use_case.dart';
+import 'package:dartz/dartz.dart';
+
+class FetchNewestBooksUseCase extends UseCase<List<BookEntity>,NoParameters>{
+  final HomeRepo homeRepo;
+
+  FetchNewestBooksUseCase({required this.homeRepo});
+  @override
+  Future<Either<Failure, List<BookEntity>>> call([NoParameters? param]) {
+    return homeRepo.fetchNewestBooks();
+  }
+
+}
