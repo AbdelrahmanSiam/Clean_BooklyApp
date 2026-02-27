bc0a76d7ed8958a170141015c1a4d71e052f9a44
Abdulrahman <syamabdo382@gmail.com>
2026-02-27 16:58:56 +0200

    impl fetchNewestBooks method in cubit

--- lib/Features/home/presentation/manager/newest_books/newest_books_cubit.dart
+++ lib/Features/home/presentation/manager/newest_books/newest_books_cubit.dart
@@ -1,9 +1,27 @@
 import 'package:bloc/bloc.dart';
 import 'package:clean_arch_bookly/Features/home/domain/entites/book_entity.dart';
+import 'package:clean_arch_bookly/Features/home/domain/use_cases/fetch_newest_books_use_case.dart';
+import 'package:clean_arch_bookly/core/errors/failure.dart';
+import 'package:dartz/dartz.dart';
 import 'package:meta/meta.dart';
 
 part 'newest_books_state.dart';
 
 class NewestBooksCubit extends Cubit<NewestBooksState> {
-  NewestBooksCubit() : super(NewestBooksInitialState());
+  NewestBooksCubit(this.fetchNewestBooksUseCase)
+    : super(NewestBooksInitialState());
+  final FetchNewestBooksUseCase fetchNewestBooksUseCase;
+  Future<void> fetchNewestBooks() async {
+    emit(NewestBooksLoadingState());
+    Either<Failure, List<BookEntity>> result = await fetchNewestBooksUseCase
+        .call();
+    result.fold(
+      (error) {
+        emit(NewestBooksFailureState(errMessage: error.errMessage));
+      },
+      (books) {
+        emit(NewestBooksSuccessState(books: books));
+      },
+    );
+  }
 }
