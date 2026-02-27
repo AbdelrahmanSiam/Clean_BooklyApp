4f0c9a52d581889293455d78e3dae4aaf8a42fa2
Abdulrahman <syamabdo382@gmail.com>
2026-02-27 16:52:55 +0200

    impl fetchFeaturedBooks in cubit

--- lib/Features/home/presentation/manager/featured_books/featured_books_cubit.dart
+++ lib/Features/home/presentation/manager/featured_books/featured_books_cubit.dart
@@ -1,9 +1,27 @@
 import 'package:bloc/bloc.dart';
 import 'package:clean_arch_bookly/Features/home/domain/entites/book_entity.dart';
+import 'package:clean_arch_bookly/Features/home/domain/use_cases/fetch_featured_books_use_case.dart';
+import 'package:clean_arch_bookly/core/errors/failure.dart';
+import 'package:dartz/dartz.dart';
 import 'package:meta/meta.dart';
 
 part 'featured_books_state.dart';
 
 class FeaturedBooksCubit extends Cubit<FeaturedBooksState> {
-  FeaturedBooksCubit() : super(FeaturedBooksInitialState());
+  FeaturedBooksCubit(this.fetchFeaturedBooksUseCase)
+    : super(FeaturedBooksInitialState());
+  final FetchFeaturedBooksUseCase fetchFeaturedBooksUseCase;
+  Future<void> fetchFeaturedBooks() async {
+    emit(FeaturedBooksLoadingState());
+    Either<Failure, List<BookEntity>> result = await fetchFeaturedBooksUseCase
+        .call();
+    result.fold(
+      (error) {
+        emit(FeaturedBooksFailureState(errMessage: error.errMessage));
+      },
+      (books) {
+        emit(FeaturedBooksSuccessState(books: books));
+      },
+    );
+  }
 }
