da6d3dd7624b5ee01d0712083c3f129677522f85
Abdulrahman <syamabdo382@gmail.com>
2026-02-28 01:08:27 +0200

    refactor code with create setUpServiceLocator function

---
+++ lib/core/utils/functions/set_up_service_locator.dart
@@ -0,0 +1,18 @@
+import 'package:clean_arch_bookly/Features/home/data/data_sources/home_local_data_source.dart';
+import 'package:clean_arch_bookly/Features/home/data/data_sources/home_remote_data_source.dart';
+import 'package:clean_arch_bookly/Features/home/data/repo/home_repo_impl.dart';
+import 'package:clean_arch_bookly/core/utils/api_services.dart';
+import 'package:dio/dio.dart';
+import 'package:get_it/get_it.dart';
+
+final getIt = GetIt.instance;
+void setUpServiceLocator() {
+  getIt.registerSingleton<HomeRepoImpl>(
+    HomeRepoImpl(
+      homeLocalDataSource: HomeLocalDataSourceImpl(),
+      homeRemoteDataSource: HomeRemoteDataSourceImpl(
+        apiServices: ApiServices(Dio()),
+      ),
+    ),
+  );
+}
--- lib/main.dart
+++ lib/main.dart
@@ -9,6 +9,7 @@ import 'package:clean_arch_bookly/Features/home/presentation/manager/newest_book
 import 'package:clean_arch_bookly/constants.dart';
 import 'package:clean_arch_bookly/core/utils/api_services.dart';
 import 'package:clean_arch_bookly/core/utils/app_router.dart';
+import 'package:clean_arch_bookly/core/utils/functions/set_up_service_locator.dart';
 import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -17,6 +18,7 @@ import 'package:hive/hive.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 
 void main() async {
+  setUpServiceLocator();
   await Hive.initFlutter();
   Hive.registerAdapter(BookEntityAdapter());
   await Hive.openBox<BookEntity>(kFeaturedBox);
@@ -35,12 +37,7 @@ class Bookly extends StatelessWidget {
           create: (context) {
             return FeaturedBooksCubit(
               FetchFeaturedBooksUseCase(
-                homeRepo: HomeRepoImpl(
-                  homeLocalDataSource: HomeLocalDataSourceImpl(),
-                  homeRemoteDataSource: HomeRemoteDataSourceImpl(
-                    apiServices: ApiServices(Dio()),
-                  ),
-                ),
+                homeRepo: getIt.get<HomeRepoImpl>(),
               ),
             );
           },
@@ -49,12 +46,7 @@ class Bookly extends StatelessWidget {
           create: (context) {
             return NewestBooksCubit(
               FetchNewestBooksUseCase(
-                homeRepo: HomeRepoImpl(
-                  homeLocalDataSource: HomeLocalDataSourceImpl(),
-                  homeRemoteDataSource: HomeRemoteDataSourceImpl(
-                    apiServices: ApiServices(Dio()),
-                  ),
-                ),
+                homeRepo: getIt.get<HomeRepoImpl>(),
               ),
             );
           },
